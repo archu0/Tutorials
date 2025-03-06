b75c552a5a7257f9824e2c43df0659c3c80d4ada
Archana S <118441423+archu0@users.noreply.github.com>
2025-03-06 13:22:21 +0530

    Update project1.tf

--- project1.tf
+++ project1.tf
@@ -1,4 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
 resource "aws_instance" "terraform_master" {
   ami                         = "ami-0f2ce9ce760bd7133"
   instance_type               = "t2.micro"
