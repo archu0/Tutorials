1ccb72b9a925eb4913ce4f322ed9b2ce715ad9e0
Archana S <118441423+archu0@users.noreply.github.com>
2025-03-06 13:53:20 +0530

    Update sample1.sh

--- sample1.sh
+++ sample1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
+sudo yum update -y
+sudo yum search docker
+sudo yum info docker
+sudo yum install -y docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl status docker.service
+curl -sL https://raw.githubusercontent.com/sakit333/ansible_insta/refs/heads/main/worker_ansible_node.sh | bash
